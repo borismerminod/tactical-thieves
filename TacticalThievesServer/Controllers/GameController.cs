6bf0dc982481eff2739a49e0cb4f3e9bf9aac357
bmermino <boris.merminod@edu.univ-fcomte.fr>
2025-09-14 18:04:35 +0200

    Création d'un GameController et de ses tests

---
+++ TacticalThievesServer/Controllers/GameController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TacticalThievesServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GameController : ControllerBase
+    {
+        [HttpPost("move")]
+        public IActionResult Move()
+        {
+            return Ok(new { success = true });
+        }
+
+        [HttpPost("stealth")]
+        public IActionResult Stealth()
+        {
+            return Ok(new { success = true });
+        }
+    }
+}
--- TacticalThievesServer/Program.cs
+++ TacticalThievesServer/Program.cs
@@ -45,3 +45,5 @@ app.UseCors();
 app.MapControllers();
 
 app.Run();
+
+public partial class Program { }
